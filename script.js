862c2d11a6616a1694a9ebb0b2ec62833ef476e3
arudo <113620499+saladarse777@users.noreply.github.com>
2023-02-19 11:16:19 +0800

    js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-$('#simpleButton2').click(function() {
+/*$('#simpleButton2').click(function() {
     alert('Save both the sea and turtles through this website!');
     window.location.assign('https://www.seeturtles.org/');
-});
+});*/
+
+    $('#simpleButtton2').click(function() {
+      var answer = confirm("Save both the sea and turtles through this website!");
+  
+      if (answer) {
+        window.location.assign('https://www.seeturtles.org/');
+      } else {
+        alert("You clicked 'Cancel'");
+      }
+    });
 
 if (/Mobi/.test(navigator.userAgent)) {
     alert("This website is not supported for mobile yet, it may look jank.");
